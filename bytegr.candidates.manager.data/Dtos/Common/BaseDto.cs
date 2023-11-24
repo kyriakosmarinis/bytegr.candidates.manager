44f48c6127ea45c7b79b5b6bba296eb69a68bc15
kyriakosmarinis <kyriakos.marinis@gmail.com>
2023-11-24 05:30:28 +0200

    bytegr.candidates.manager - web (bug fixed)

--- bytegr.candidates.manager.data/Dtos/Common/BaseDto.cs
+++ bytegr.candidates.manager.data/Dtos/Common/BaseDto.cs
@@ -3,7 +3,7 @@ namespace bytegr.candidates.manager.data.Dtos.Common
 {
 	public class BaseDto
 	{
-        public DateTime DateCreated { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
     }
 }
 
